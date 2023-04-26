c43201566c7cb196f52322aaa9fd4e667420b702
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2023-04-26 09:08:20 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,60 @@
+<script>
+    import Teams from './Teams.svelte';
+
+    let redTeam = 'red'
+    let blueTeam = 'blue';
+    let redValues = 20
+    let blueValues = 20
+
+    const resetValues = () => {
+        redValues = 20;
+        blueValues = 20;
+    }
+
+</script>
+
+<main class="container">
+    <div class="row">
+        <div class="column">
+            <h1>MTG Counter App</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="column">
+            <Teams color={redTeam} bind:value={redValues}/>
+        </div>
+        <div class="column">
+            <Teams color={blueTeam} bind:value={blueValues}/>
+        </div>
+    </div>
+    <div style="text-align: center">
+        {#if blueValues === 0}
+            <h2 style="color: {redTeam}">Red Team Wins</h2>
+        {:else if redValues === 0}
+                <h2 style="color: {blueTeam}">Blue Team Wins</h2>
+        {/if}
+        {#if redValues === 0 || blueValues === 0}
+            <button style="background: green" on:click={resetValues}>New Game</button>
+            {:else}
+            <button on:click={resetValues}>Reset Game</button>
+        {/if}
+    </div>
+</main>
+
+
+<style>
+    h1 {
+        color: black;
+        font-size: 3rem;
+        font-weight: 500;
+        text-align: center;
+        line-height: none;
+    }
+    button {
+        background-color: orange;
+        border: none;
+        margin-top: .5rem;
+        width: 100%;
+    }
+
+</style>
---
+++ src/Teams.svelte
@@ -0,0 +1,67 @@
+
+<script>
+    export let color = 'red'
+    export let value = 20;
+    $: colorName = color.charAt(0).toUpperCase() + color.slice(1);
+
+    const addQuantity = () => {
+        if (value >= 20) {
+            return;
+        }
+        value += 1;
+    }
+
+    const removeQuantity = () => {
+        if (value <= 0) {
+            return;
+        }
+        value -= 1;
+    }
+
+</script>
+
+<div class="container pa">
+    <div style="border: dashed 5px {color}" class="team">
+        <h4 style="color: {color}">Team {colorName}: {value}</h4>
+    <div>
+        <button class="plus-button" on:click={addQuantity}>+</button>
+        <button class="minus-button" on:click={removeQuantity}>-</button>
+    </div>
+    </div>
+</div>
+
+<style>
+    * {
+        padding: 0;
+        margin: 0;
+    }
+
+    .team {
+        width: 100%;
+        min-height: 100px;
+        padding: 1rem;
+        font-weight: 700;
+        text-align: center;
+        border: dotted px red;
+    }
+    h2 {
+        text-align: center;
+    }
+    h4 {
+        font-weight: 500;
+    }
+    button {
+        width: 30px;
+        height: 30px;
+        padding: .5rem;
+        border: none;
+    }
+    .plus-button {
+        line-height: 0;
+        background-color: green;
+    }
+    .minus-button {
+        line-height: 0;
+        background-color: red;
+    }
+</style>
